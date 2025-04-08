c2232140f57eaf3c6ebc2df72959de22e7cb547a
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 16:12:16 +0100

    Added an SNS

--- main.tf
+++ main.tf
@@ -1,3 +1,34 @@
+resource "aws_sns_topic" "topic" {
+  name = "s3-event-notification-topic"
+
+  policy = <<POLICY
+{
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Effect": "Allow",
+        "Principal": {"AWS":"*"},
+        "Action": "SNS:Publish",
+        "Resource": "arn:aws:sns:*:*:s3-event-notification-topic"
+    }]
+}
+POLICY
+}
+
+
+resource "aws_s3_bucket" "my_objects" {
+  bucket = "ourcorp"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = "${aws_s3_bucket.my_objects.id}"
+
+  topic {
+    topic_arn     = "${aws_sns_topic.topic.arn}"
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".log"
+  }
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
