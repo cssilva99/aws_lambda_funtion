ffdf6d9d2c9f975c8985b34f6c2f6583c8de65d1
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 16:33:31 +0100

    attempt

--- main.tf
+++ main.tf
@@ -1,32 +1,19 @@
-resource "aws_sns_topic" "topic" {
-  name = "s3-event-notification-topic"
+# ----------------------------------------------------------------------------------------------------------------------
+# CREATE A S3 BUCKET AND UPLOAD THE ARCHIVE AS AN OBJECT
+# ----------------------------------------------------------------------------------------------------------------------
 
-  policy = <<POLICY
-{
-    "Version":"2012-10-17",
-    "Statement":[{
-        "Effect": "Allow",
-        "Principal": {"AWS":"*"},
-        "Action": "SNS:Publish",
-        "Resource": "arn:aws:sns:*:*:s3-event-notification-topic"
-    }]
-}
-POLICY
+module "s3_bucket" {
+  source  = "mineiros-io/s3-bucket/aws"
+  version = "~> 0.5.0"
+
+  bucket_prefix = "mineiros-s3-example-"
+  force_destroy = true
 }
 
-
-resource "aws_s3_bucket" "my_objects" {
-  bucket = "ourcorp"
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = "${aws_s3_bucket.my_objects.id}"
-
-  topic {
-    topic_arn     = "${aws_sns_topic.topic.arn}"
-    events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
-  }
+resource "aws_s3_bucket_object" "function" {
+  bucket = module.s3_bucket.id
+  key    = "main.py.zip"
+  source = data.archive_file.lambda.output_path
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -50,8 +37,8 @@ EOF
 }
 
 resource "aws_lambda_function" "this" {
-  s3_bucket     = "${aws_s3_bucket.my_objects}"
-  s3_key        = "${aws_s3_bucket.my_objects.id}"
+  s3_bucket = aws_s3_bucket_object.bucket
+  s3_key    = aws_s3_bucket_object.function.key
   function_name = "lambda_function_name"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "exports.test"
