65d3d367d41d470ece52a3fc2241be1aeb1408a8
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 16:03:59 +0100

    test

--- main.tf
+++ main.tf
@@ -19,7 +19,6 @@ EOF
 }
 
 resource "aws_lambda_function" "this" {
-  filename      = "get_joke_lambda_function.zip"
   function_name = "lambda_function_name"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "exports.test"
