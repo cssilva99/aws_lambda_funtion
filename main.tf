939800861c258d75d8df391e6afacd8904b956af
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 17:01:04 +0100

    fdhlfjls

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ EOF
 }
 
 resource "aws_lambda_function" "this" {
-  s3_bucket = "ourcorp"
+  s3_bucket = "myportfoliodescription"
   s3_key    = aws_s3_bucket_object.function.key
   function_name = "lambda_function_name"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
